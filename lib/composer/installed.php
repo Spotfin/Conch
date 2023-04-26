3367074e610fd91ab758b6666ded82ff5c007199
Drew Poland <1743054+bmoredrew@users.noreply.github.com>
2023-04-26 08:34:07 -0400

    move vendor dir

---
+++ lib/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spotfin/conch',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5637d64eb8dabb3fcd736df9ea3fe229e34f6a9',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spotfin/conch' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5637d64eb8dabb3fcd736df9ea3fe229e34f6a9',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
